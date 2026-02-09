9507acdbeb2315097f514f7cf128ea45958c258e
Brandon Vo <u1457332@umail.utah.edu>
2026-02-08 21:30:39 -0700

    LED configure

--- lab2/Src/lab2.c
+++ lab2/Src/lab2.c
@@ -17,6 +17,11 @@ int main(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_SYSCFG_CLK_ENABLE();
   GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
   while (1)
   {
  
